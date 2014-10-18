a8e4a43943563b189d4d6ceaacc098e282351116
Maikel Steneker <maikel.steneker@gmail.com>
2014-10-18 16:54:40 +0200

    Added interface, robustness.

--- mastermind.py
+++ mastermind.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
-n_colours = 9
-n_pegs = 4
-unique = True
-colour_names = ['Colour %d' % i for i in range(n_colours)]
+import argparse
+
+parser = argparse.ArgumentParser(description='Solve a mastermind game.', epilog='If no options are provided, the original rules are used.')
+parser.add_argument('colour_names', metavar='colour-names', type=str, nargs='*',
+                    help='names for the colours of the pegs (defaults to numbers)')
+parser.add_argument('-c', '--colours', type=int, default=6,
+                    help='the number of different colours')
+parser.add_argument('-p', '--pegs', type=int, default=4,
+                    help='the number of pegs')
+parser.add_argument('-u', '--unique', dest='unique', action='store_true',
+                    help='Allow non-unique solutions.')
+parser.add_argument('-n', '--numbers', dest='numbers', action='store_true',
+                    help='Use numbers instead of colours')
+args = parser.parse_args()
+
+n_colours = args.colours
+n_pegs = args.pegs
+unique = args.unique
+colour_names = ['Colour %d' % (i+1) for i in range(n_colours)]
+if all((args.colour_names==[], n_colours == 6, args.pegs == 4, not unique, not args.numbers)):
+    # default rules
+    colour_names = ['red', 'yellow', 'green', 'blue', 'white', 'black']
+for i in range(len(args.colour_names)):
+    colour_names[i] = args.colour_names[i]
+if args.numbers:
+    colour_names = ['%d' % (i+1) for i in range(n_colours)]
+
 pretty_guess = lambda guess: ' '.join(colour_names[i] for i in guess)
 
 def generate_all_solutions(n_colours=n_colours, n_pegs=n_pegs, unique=unique, forbidden=[]):
@@ -64,13 +87,16 @@ if __name__=='__main__':
     
     done = False
     hints = []
-    next_guess = list(range(n_pegs))
+    next_guess = list(range(n_pegs)) if unique else [0, 0, 1, 1]
     try:
         while not done:
             print('Next guess: {0}'.format(pretty_guess(next_guess)))
-            hints.append((next_guess,
-                (int(input('Number of pegs on the correct location: ')),
-                 int(input('Number of correct pegs in the wrong location: ')))))
+            try:
+                hints.append((next_guess,
+                    (int(input('Number of pegs on the correct location: ')),
+                     int(input('Number of correct pegs in the wrong location: ')))))
+            except ValueError:
+                print('Please provide a valid number.')
             generator = generate_solutions(hints=hints)
             next_guess = next(generator)
             try:
@@ -78,6 +104,9 @@ if __name__=='__main__':
             except StopIteration:
                 done = True
         solution = next_guess
-        print('Solution: {0}'.format(solution))
+        print('Solution: {0}'.format(pretty_guess(solution)))
     except StopIteration:
         print('No solution that satisfies the provided hints!')
+    except EOFError:
+        print()
+        print('Input ended before solution could be found!')
